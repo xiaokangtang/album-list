1f7d3b3425fb00f6c98f62820789f73cbb012fbe
Xiaokang Tang <xiaokangt@hotmail.com>
2022-05-16 20:19:37 -0400

    gh & readme

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+  <h2 align="center">Album list</h2>
+  <p align="center">
+    A playground for tailwind & storybook. Using React context for state management.
+    <br />
+    <br />
+    <a href="https://xiaokangtang.github.io/album-list">View Demo Here</a>
+  </p>
+</p>
+
+## About The Project
+
+### Built With
+
+- React 17
+
+- Tailwind 2.0
+
+- Storybook
--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,10 +1,10 @@
 const ListItem = ({ name, image, price, artist, rights }) => {
   return (
-    <div className="w-full lg:max-w-full lg:flex">
-      <div className="h-48 lg:h-auto flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+    <div className="w-full md:max-w-full md:flex">
+      <div className="h-48 md:h-auto flex-none bg-cover rounded-t md:rounded-t-none md:rounded-l text-center overflow-hidden">
         <img className="h-auto max-w-full" src={image} alt={name}></img>
       </div>
-      <div className="lg:w-full border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+      <div className="md:w-full border-r border-b border-l border-gray-400 md:border-l-0 md:border-t md:border-gray-400 bg-white rounded-b md:rounded-b-none md:rounded-r p-4 flex flex-col justify-between leading-normal">
         <h2>{name}</h2>
         <div>
           {/* <p>Artist(s): {artist} </p> */}
